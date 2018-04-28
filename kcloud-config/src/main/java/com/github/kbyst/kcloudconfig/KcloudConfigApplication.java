1e6180e438804f4e64b3f523f28cf9770888fbdb
kbyst <jjy6JJY^>
2018-04-28 14:38:40 +0800

    create

---
+++ kcloud-config/src/main/java/com/github/kbyst/kcloudconfig/KcloudConfigApplication.java
@@ -0,0 +1,12 @@
+package com.github.kbyst.kcloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcloudConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcloudConfigApplication.class, args);
+	}
+}
---
+++ kcloud-registry/src/main/java/com/github/kbyst/kcloudregistry/KcloudRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.github.kbyst.kcloudregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcloudRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcloudRegistryApplication.class, args);
+	}
+}
