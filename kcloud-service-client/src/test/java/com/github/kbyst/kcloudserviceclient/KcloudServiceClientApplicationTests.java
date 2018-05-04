40ce76a0adba83a29995b27df8826d8add4336ee
kbyst <jjy6JJY^>
2018-05-04 10:20:55 +0800

    service

---
+++ kcloud-service-client/src/test/java/com/github/kbyst/kcloudserviceclient/KcloudServiceClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.kbyst.kcloudserviceclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KcloudServiceClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
