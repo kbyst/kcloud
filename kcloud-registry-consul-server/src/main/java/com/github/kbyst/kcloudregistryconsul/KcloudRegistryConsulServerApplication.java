2bd807bd94d08cc0313246b340d5fdee1c7bd6af
kbyst <jjy6JJY^>
2018-05-18 14:30:14 +0800

    kcloud

--- kcloud-registry-consul-server/src/main/java/com/github/kbyst/kcloudregistryconsul/KcloudRegistryConsulServerApplication.java
+++ kcloud-registry-consul-server/src/main/java/com/github/kbyst/kcloudregistryconsul/KcloudRegistryConsulServerApplication.java
@@ -1,12 +1,15 @@
 package com.github.kbyst.kcloudregistryconsul;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableDiscoveryClient
 @RestController
 public class KcloudRegistryConsulServerApplication {
